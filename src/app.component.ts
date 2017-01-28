16fd8538f58ca735af6a1707d9d5376c63b9525d
Bhisham Singh <bhisam2108@gmail.com>
2017-01-28 21:21:16 +0530

    initial commit

---
+++ src/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app',
+  templateUrl: "../public/templates/app.html"
+})
+export class AppComponent  { name = 'Angular 2'; }
